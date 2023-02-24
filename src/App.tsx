7afd49fc1b1aae272e419ff89978e1e62585a166
iSonce <mail@sonce.me>
2023-02-24 08:07:18 +0800

    fix: text wraps correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState } from 'react'
 import chat from './api/chat'
 // import example from './common/example'
 import {
@@ -10,15 +10,14 @@ import {
   ListItemText,
   Avatar,
   Button,
-  FormGroup,
 } from '@mui/material'
 import SendIcon from '@mui/icons-material/Send'
 import './App.css'
 
-function App() {
-  const SONCE_AVATARS = import.meta.env.VITE_APP_SONCE_AVATARS_URL
-  const OPENAI_AVATARS = import.meta.env.VITE_APP_OPENAI_AVATARS_URL
+const SONCE_AVATARS = import.meta.env.VITE_APP_SONCE_AVATARS_URL
+const OPENAI_AVATARS = import.meta.env.VITE_APP_OPENAI_AVATARS_URL
 
+function App() {
   const [textInput, setTextInput] = useState<string>('')
   const [messageList, setMessageList] = useState<Message[]>([])
   const [conversationId, setConversationId] = useState<string>(
@@ -54,23 +53,22 @@ function App() {
     setTextInput('')
   }
   return (
-    <div className="overflow-hidden">
-      <Box className="overflow-y-auto h-100vh scrollbar">
-        <List className="">
+    <div className="overflow-hidden" id="app">
+      <Box className="overflow-y-auto h-92vh scrollbar">
+        <List>
           {messageList.map((message, idx) => (
-            <ListItem key={idx}>
+            <ListItem key={idx} alignItems="flex-start" className="min-w-600px">
               <ListItemAvatar>
                 <Avatar src={idx % 2 ? OPENAI_AVATARS : SONCE_AVATARS} />
               </ListItemAvatar>
-              {/* TODO: 换行文字处理 */}
-              <ListItemText disableTypography={true}>
+              <ListItemText className="ws-pre-wrap">
                 {message.text}
               </ListItemText>
             </ListItem>
           ))}
         </List>
       </Box>
-      <Box className="flex absolute bottom-0 z-100 bg-#FFF p-1/100 w-100vw">
+      <Box className="flex absolute bottom-0 z-100 bg-#FFF p-1/100 w-100vw h-8vh">
         <TextField
           value={textInput}
           onChange={(e) => setTextInput(e.target.value)}
